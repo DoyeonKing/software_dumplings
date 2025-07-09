70508e11ea22175a07605d37298146568dac751d
OnionHead12333 <2518293611@qq.com>
2025-07-09 10:06:09 +0800

    大扫除

---
+++ bicycle_sharing/springboot/src/Mybatis_plus.java
@@ -0,0 +1,5 @@
+gc.setAuthor("ww");
+dsc.setUrl("jdbc:mysql://localhost:3306/springboot?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
