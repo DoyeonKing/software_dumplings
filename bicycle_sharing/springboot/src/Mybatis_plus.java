368aed7ce496913157a34e96d6f251c61d6a413d
DoyeonKing <23301153@bjtu.edu.cn>
2025-07-09 11:13:16 +0800

    Revert "大扫除"
    This reverts commit 70508e11ea22175a07605d37298146568dac751d.

--- bicycle_sharing/springboot/src/Mybatis_plus.java
+++
@@ -1,5 +0,0 @@
-gc.setAuthor("ww");
-dsc.setUrl("jdbc:mysql://localhost:3306/springboot?useUnicode=true&useSSL=false&characterEncoding=utf8");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("root");
