e2a6903e46524c5bf61705629d5b580fc4fa63c8
DoyeonKing <23301153@bjtu.edu.cn>
2025-07-14 10:05:30 +0800

    重新修改

--- bicycle_sharing/springboot/src/main/java/com/example/springboot/controller/EliteSitesController.java
+++ bicycle_sharing/springboot/src/main/java/com/example/springboot/controller/EliteSitesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/geohashInfo") // 定义这个控制器的基础URL路径
-public class GeohashInfoController {
+public class EliteSitesController {
 
     @Resource
     private IEliteSitesService geohashInfoService;
