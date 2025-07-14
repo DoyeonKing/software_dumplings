313560ec66c0c474cb629bb0728138e4acf3ac75
DoyeonKing <23301153@bjtu.edu.cn>
2025-07-14 16:32:52 +0800

    完善了geohashInfo数据接收问题

--- bicycle_sharing/springboot/src/main/java/com/example/springboot/config/WebConfig.java
+++ bicycle_sharing/springboot/src/main/java/com/example/springboot/config/WebConfig.java
@@ -45,6 +45,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "/route/**",        // 路线规划接口
                         "/elite-sites/**",  // 精华站点接口
                         "/dispatchTasks/**", // 调度任务接口
+                        "/geohashInfo/**", // Geohash信息接口
 
                         // 其他
                         "/files/**",        // 文件服务
